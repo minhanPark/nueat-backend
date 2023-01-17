266202a12b0446f7354919623de3cf20750389b9
minhanPark <alsgks1218@naver.com>
2023-01-17 11:56:54 +0900

    mail.service까지 테스트

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test } from '@nestjs/testing';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
+
+const TEST_KEY = 'testKey';
+const USER_ID = 1;
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+    verify: jest.fn(() => ({ id: USER_ID })),
+  };
+});
+
+describe('JWTService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const token = service.sign(USER_ID);
+      expect(token).toBe('TOKEN');
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: USER_ID }, TEST_KEY);
+    });
+  });
+  describe('verify', () => {
+    it('should return the decoded token', () => {
+      const TOKEN = 'TOKEN';
+      const decodedToken = service.verify(TOKEN);
+      expect(decodedToken).toEqual({ id: USER_ID });
+      expect(jwt.verify).toHaveBeenCalledWith(TOKEN, TEST_KEY);
+    });
+  });
+});
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -11,11 +11,11 @@ export class MailService {
   ) {
     //this.sendEmail('test', 'test email')
   }
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     emailVars: EmailVar[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', 'alsgks1218@naver.com');
@@ -23,17 +23,20 @@ export class MailService {
     form.append('template', template);
     emailVars.forEach((evar) => form.append(`v:${evar.key}`, evar.value));
     try {
-      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+      await got.post(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      });
+      );
+      return true;
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
