ecff64ac77d00ace99c59ee347185ca3630a6186
minhanPark <alsgks1218@naver.com>
2023-01-19 11:45:09 +0900

    에러수정

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -20,7 +20,7 @@ export class JwtMiddleware implements NestMiddleware {
         const token = req.headers['x-jwt'];
         const decoded = this.jwtService.verify(token.toString());
         if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-          const user = await this.userService.findById(decoded['id']);
+          const { user } = await this.userService.findById(decoded['id']);
           req['user'] = user;
         }
       } catch (e) {}
